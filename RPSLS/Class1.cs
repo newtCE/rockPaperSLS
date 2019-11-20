37b8e7c0bff4258a1e741e2a380e1a8f3d8849ca
Newt Industries <info@newtindustries.com>
2019-11-20 13:34:38 -0600

    first push, began player class and game loop, selecting game modes functional

---
+++ RPSLS/Class1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS
+{
+    class GameLoop
+    {
+        public int currentRound;
+        public string gameMode;
+        public string player2Type;
+
+        public void SelectGameMode()
+        {
+            Console.WriteLine("Please select the game mode you'd like to play:\n1.)Play Against Computer\n2.)Play Against A Human\n3.)Quit Game");
+            gameMode = (Console.ReadLine());
+            if (gameMode == "1" || gameMode == "2" || gameMode == "3")
+            {
+                switch (gameMode)
+                {
+                        case "1":
+                        player2Type = "cpu";
+                        CreateBout(player2Type);
+                        break;
+
+                        case "2":
+                        player2Type = "human";
+                        CreateBout(player2Type);
+                        break;
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Not a Valid option, please type either 1, 2 or 3...");
+                Console.ReadLine();
+                SelectGameMode();
+            }
+        }
+        public void CreateBout(string player2Type)
+        {
+            Player player1 = new Player("human");
+            Player player2 = new Player(player2Type);
+        }
+
+
+    }
+}
---
+++ RPSLS/Class2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS
+{
+    class Player
+    {
+        public string playerNumber;
+        public int boutScore;
+
+        public string SelectGesture(string gestureThrown)
+        {
+            public string outPutGesture;
+            switch (gestureThrown)
+            {
+                case "1":
+                outPutGesture="Rock";
+                break;
+                case "2":
+                outPutGesture="Paper";
+                break;
+                case "3":
+                outPutGesture="Scissors";
+                break;
+                case "4":
+                outPutGesture="Lizard";
+                break;
+                case "5":
+                outPutGesture="Spock";
+                break;
+            }
+        }
+    }
+}
